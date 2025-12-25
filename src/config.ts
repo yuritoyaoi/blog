599f3d84ab74db009946240d0a2ab5b4fa23c2d7
Yuri to Yaoi <transinjapan@proton.me>
2025-12-26 07:49:01 +0900

    updated banner and favicon

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: '/assets/images/header.jpg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: 'assets/images/header.jpg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -32,7 +32,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// Leave this array empty to use the default favicon
 		 {
-		   src: 'public/favicon/logo.png',    // Path of the favicon, relative to the /public directory
+		   src: 'favicon/logo.png',    // Path of the favicon, relative to the /public directory
 		  // theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 		   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		 }
