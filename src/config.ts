ea897c001b388dab6645f4ff928b0b6ad3d57e29
Yuri to Yaoi <transinjapan@proton.me>
2025-12-28 00:00:10 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -70,7 +70,7 @@ export const profileConfig: ProfileConfig = {
 			icon: "fa6-brands:discord", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://discord.gg/8RdCRMGNYQ",
+			url: "https://discord.gg/hgrdx8hFaA",
 		},
 	],
 };
