7660451327c5567bb6bf5606e18a87c2c0578364
Yuri to Yaoi <transinjapan@proton.me>
2025-12-27 19:29:41 +0900

    edit rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -32,7 +32,7 @@ export async function GET(context: APIContext) {
 				pubDate: post.data.published,
 				description: post.data.description || "",
 				link: url(`/posts/${post.slug}/`),
-				category: post.data.category | "",
+				category: post.data.category || "",
 				content: sanitizeHtml(parser.render(cleanedContent), {
 					allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
 				}),
